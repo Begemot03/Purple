038f84cd7e6a8b19bf462bcd527f51fee0a43cb2
Andre <andre.petrovto@mail.ru>
2024-12-10 00:31:00 +0300

    feat: stylize notFound page

--- src/pages/notFound/NotFoundPage.tsx
+++ src/pages/notFound/NotFoundPage.tsx
@@ -1,3 +1,15 @@
 import { FC } from "react";
 
-export const NotFoundPage: FC = () => <div>404</div>;
+export const NotFoundPage: FC = () => {
+	return (
+		<div className="fullscreen auto-center">
+			<div className="not-found">
+				<h1 className="not-found__title">404</h1>
+				<p className="not-found__subtitle">Страница не найдена</p>
+				<a href="/" className="btn btn-transp not-found__btn">
+					На главную
+				</a>
+			</div>
+		</div>
+	);
+};
